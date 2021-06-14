e1bfc556b2c715dab5f98c0dbc4c4e97dd689b76
Aswin Balakrishnan <84726851+AswinBalakrishnan@users.noreply.github.com>
2021-06-14 21:18:40 +0530

    change 2

--- solns.c
+++ solns.c
@@ -8,3 +8,11 @@ int max(int a[], int n){
      printf("max is %i \n",large);}
        return large;
        }
+int min(int a[], int n){
+      int small=a[0];
+    for(int i=0;i<n;i++){
+      if (small>a[i])
+       small=a[i];
+     printf("min is %i \n",small);}
+       return small;
+       }
