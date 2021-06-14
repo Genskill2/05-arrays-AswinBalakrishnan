9e7980c40c96fb24ca51d28969a298e78aba2279
Aswin Balakrishnan <84726851+AswinBalakrishnan@users.noreply.github.com>
2021-06-14 21:09:56 +0530

    change1

--- solns.c
+++ solns.c
@@ -1,2 +1,10 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
+int max(int a[], int n){
+      int large=a[0];
+    for(int i=0;i<n;i++){
+      if (a[i]>large)
+       large=a[i];
+     printf("max is %i \n",large);}
+       return large;
+       }
